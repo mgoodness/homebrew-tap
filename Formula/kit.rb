7e01b6b3afb247dff2d73b014b7abf8d7b542881
Michael Goodness <mike@opsgoodness.net>
2026-09-09 14:59:25 +0000

    kit: add 0.105 bottle.

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -5,6 +5,12 @@ class Kit < Formula
   sha256 "962834795e812a9546daee438b71815d7d3dcb97a05a955f9d5fead573e536d1"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/mgoodness/homebrew-tap/releases/download/kit-0.105"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "dbfe703a894d090aabb43cf0f03a6137a18aa7b7161ed8aab4f459331d4188d8"
+    sha256 cellar: :any,                 x86_64_linux: "36f9fb00aa320c5922227b47b34ade099f485348ee5ebbb7ebf29185aa78773a"
+  end
+
   depends_on "go" => :build
 
   def install
